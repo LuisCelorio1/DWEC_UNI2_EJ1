5632cd67e027760a3656def9afb6aad3ffce3b7e
LuisCelorio1 <luisec85@educastur.es>
2023-09-29 14:19:44 +0200

    aa

---
+++ js/ejercicio1.js
@@ -0,0 +1,16 @@
+var nombreApellidos = prompt("Escriba su nombre y apellidos");
+
+
+function minuscula(nombreM){
+    return nombreM.toLowerCase();
+}
+
+console.log(minuscula(nombreApellidos));
+
+function mayuscula(nombreM){
+    return nombreM.toUpperCase();
+}
+console.log(mayuscula(nombreApellidos));
+
+
+
